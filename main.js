82ef575e1db154784a87f4178dc0e85d3140c12a
Clay Schneider <clayms345@gmail.com>
2022-05-11 23:46:03 -0700

    some work done

--- main.js
+++ main.js
@@ -3,11 +3,29 @@ document.getElementById('add').addEventListener('click', addItem)
 const frozen = document.createElement('p')
 
 
+class Item {
+    constructor(name){
+        this._name = name
+        this._bought = false
+    }
+    get name() {
+        return this._name
+    }
+    get bought() {
+        return this._bought
+    }
+
+    isBought(){
+        this._bought = true
+        return
+    }
+}
+
 function addItem() {
     let choice = document.getElementById('categories')
     let dropDown = choice.options[choice.selectedIndex].value
     let input = document.querySelector('input').value
     if (dropDown === 'Frozen') {
-        document.getElementById('Frozen').appendChild(frozen).innerText = input
+        document.getElementById('Frozen').appendChild(frozen).innerText += input +"\n "
     }
 }
